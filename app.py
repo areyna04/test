5fdc0606f6cf9593e96c7169ac31b5e0f2c6621f
Arturo Reyna <a.reyna@uniandes.edu.co>
2023-04-23 15:33:37 -0600

    rr

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ app = Flask(__name__)
 
 @app.route('/api/tasks', methods=['GET', 'POST'])
 def tasks():
-    if request.method == 'POST':
-        return jsonify({'message': 'Datos recibidos correctamente.'})
-    elif request.method == 'GET':
-        return jsonify({'message': 'Esto Funciona'})
+    return jsonify({'message': 'Esto Funciona'})
 
 if __name__ == '__main__':
     app.run(debug=True)

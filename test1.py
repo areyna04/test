e9a34c5016a163e97581ca1b758298007d66bc66
Arturo Reyna <a.reyna@uniandes.edu.co>
2023-04-23 14:51:47 -0600

    s

--- test1.py
+++ test1.py
@@ -9,7 +9,7 @@ err = 0
 
 print("Escenario 1")
 while True:
-    cmd = f"ab -n {concurrent_requests} -c {concurrent_requests} -g output1.csv ulimit -n 10000 {url}"
+    cmd = f"ab -n {concurrent_requests} -c {concurrent_requests} -g output1.csv ulimit {url}"
     print(cmd)
     result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
     tpr = None

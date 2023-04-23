228d98f9b9485cf0856de3a71bc7f64bdddcd287
Arturo Reyna <a.reyna@uniandes.edu.co>
2023-04-23 12:12:06 -0600

    ssss

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import subprocess
 
-url = "https://107.23.134.168:5000/api/tasks"
+url = "https://localhost:5000/api/tasks"
 
 cmd = f"ab -n 100 -c 10 {url}"
 result = subprocess.run(cmd, shell=True, capture_output=True, text=True)
